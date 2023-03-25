2f479d5d1aabd5b358010d886424bf03e956174f
iochkolovski <poodsfan29@gmail.com>
2023-03-25 18:40:56 +0200

    test

---
+++ apium-testing/apium-testing/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace apium_testing
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
