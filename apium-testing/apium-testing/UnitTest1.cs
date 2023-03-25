7acc9d1d648d2806463d1b1155eb193201be72a4
iochkolovski <poodsfan29@gmail.com>
2023-03-25 19:30:46 +0200

    test

--- apium-testing/apium-testing/UnitTest1.cs
+++ apium-testing/apium-testing/UnitTest1.cs
@@ -1,16 +1,35 @@
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
 namespace apium_testing
 {
     public class Tests
     {
-        [SetUp]
+        private const string AppiumServerUri = "http://localhost:8080/wd/hub/";
+        private const string SummatorAppPath = @"C:\Users\poods\Desktop\softuni!\SummatorDesktopApp.exe";
+        private WindowsDriver<WindowsElement> driver;
+
+        [OneTimeSetUp]
         public void Setup()
         {
+            var appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability("PlatformName", "Windows");
+            appiumOptions.AddAdditionalCapability("app", SummatorAppPath);
+            driver = new WindowsDriver<WindowsElement>(new Uri(AppiumServerUri), appiumOptions);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Quit();
         }
 
         [Test]
-        public void Test1()
+        public void Test_CanSum_Two_PositiveNumbers()
         {
-            Assert.Pass();
+            var firstFieldNum = driver.FindElementByAccessibilityId("textBoxFirstNum");
+            firstFieldNum.Clear();
         }
     }
 }
